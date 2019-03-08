323ea1ca2d7a491afcd603e8adff6faf2ea04315
robervip <44229332+robervip@users.noreply.github.com>
2019-03-08 20:34:27 +0100

    07

---
+++ reguistro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="listadoUsuarios.php" method="post">
+      Nombre: <input type="text" name="nombre" value=""><br><br>
+      Apellido: <input type="text" name="apellido" value=""><br><br>
+      Usuario: <input type="text" name="usuario" value="">
+      <input type="submit" name="" value="Enviar">
+    </form>
+  </body>
+</html>
